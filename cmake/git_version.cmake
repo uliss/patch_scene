087a85ae2a3e793597d90657075b7c9b898ff943
Serge Poltavski <serge.poltavski@gmail.com>
2024-08-21 02:09:24 +0300

    file format version

--- cmake/git_version.cmake
+++ cmake/git_version.cmake
@@ -1,5 +1,7 @@
 find_package(Git)
 
+set(PATCH_SCENE_FILE_FORMAT_VERSION 2)
+
 add_custom_target(git_version
     ${CMAKE_COMMAND}
         -D SRC=${CMAKE_SOURCE_DIR}/patch_scene_version.h.in
@@ -9,6 +11,7 @@ add_custom_target(git_version
         -D PATCH_SCENE_VERSION_MAJOR=${PROJECT_VERSION_MAJOR}
         -D PATCH_SCENE_VERSION_MINOR=${PROJECT_VERSION_MINOR}
         -D PATCH_SCENE_VERSION_PATCH=${PROJECT_VERSION_PATCH}
+        -D PATCH_SCENE_FILE_FORMAT_VERSION=${PATCH_SCENE_FILE_FORMAT_VERSION}
         -P ${CMAKE_SOURCE_DIR}/cmake/git_version_generate_header.cmake
 )
 
