20f042d9478e50a99347394776ba2e1d7b55f684
Serge Poltavski <serge.poltavski@gmail.com>
2024-08-22 03:31:56 +0300

    win: deploy using mingw-ldd

---
+++ scripts/mingw_deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+BIN_DIR="@CMAKE_INSTALL_PREFIX@/@CMAKE_INSTALL_BINDIR@"
+APP_EXE="${BIN_DIR}/@PROJECT_NAME@.exe"
+MINGW_LDD=$(which mingw-ldd)
+MINGW_BIN_DIR=$(dirname ${MINGW_LDD})
+windeployqt6 "${APP_EXE}"
+${MINGW_LDD} "${APP_EXE}" --dll-lookup-dirs "${BIN_DIR}" ${MINGW_BIN_DIR} \
+    | grep -v 'not found' \
+    | cut -d'>' -f2 \
+    | grep -v 'Qt6' \
+    | sed '/^$/d' \
+    | while read x
+    do
+        cp -v "$x" "${BIN_DIR}/"
+    done
