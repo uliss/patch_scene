280bb0dc88c1e2862dd6f53a9df1527551368bc9
Serge Poltavski <serge.poltavski@gmail.com>
2024-08-23 01:09:39 +0300

    win: plugin dll dep fixes

--- scripts/mingw_deploy.sh
+++ scripts/mingw_deploy.sh
@@ -5,16 +5,39 @@ APP_EXE="${BIN_DIR}/@PROJECT_NAME@.exe"
 MINGW_LDD=$(which mingw-ldd)
 MINGW_BIN_DIR=$(dirname ${MINGW_LDD})
 windeployqt6 "${APP_EXE}"
-${MINGW_LDD} "${APP_EXE}" --dll-lookup-dirs "${BIN_DIR}" ${MINGW_BIN_DIR} \
-    | grep -v 'not found' \
-    | cut -d'>' -f2 \
-    | grep -v 'Qt6' \
-    | sed '/^$/d' \
-    | while read x
-    do
-        cp -v "$x" "${BIN_DIR}/"
-    done
 
+dll_list() {
+    ${MINGW_LDD} "$1" --dll-lookup-dirs "${BIN_DIR}" ${MINGW_BIN_DIR} \
+        | grep -v 'not found' \
+        | cut -d'>' -f2 \
+        | grep -v 'Qt6' \
+        | sed '/^$/d'
+}
+
+echo "- fix main app DLL deps ..."
+dll_list "${APP_EXE}" | while read x
+do
+    if [ "$(dirname $x)" != "${BIN_DIR}" ]; then
+        cp -v "$x" "${BIN_DIR}/"
+    fi
+done
+
+echo "- fix plugins DLL deps ..."
+ls -d ${BIN_DIR}/*/ | while read dir
+do
+    echo "- processing directory $(basename $dir) ..."
+    find $dir -name '*.dll' | while read dll
+    do
+        dll_list $dll | while read dep_dll
+        do
+            if [ ! -f "${BIN_DIR}/$(basename $dep_dll)" ]; then
+                cp -v "$dep_dll" "${BIN_DIR}/"
+            fi
+        done
+    done
+done
+
+echo "- creating qt.conf ..."
 cat << 'EOF' > ${BIN_DIR}/qt.conf
 [Paths]
 Prefix = .
