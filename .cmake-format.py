c844b0e1c1355c32957f31a4f5d5d2a70c728445
Serge Poltavski <serge.poltavski@gmail.com>
2024-08-28 18:26:16 +0300

    cmake format

---
+++ .cmake-format.py
@@ -0,0 +1,8 @@
+# -----------------------------
+# Options effecting formatting.
+# -----------------------------
+with section("format"):
+  # How wide to allow formatted cmake files
+  line_width = 100
+  # How many spaces to tab for indent
+  tab_size = 4
