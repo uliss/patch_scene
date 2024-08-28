9006cd8d87393350dfe613de59f57ca5011e59ca
Serge Poltavski <serge.poltavski@gmail.com>
2024-08-28 21:56:28 +0300

    update blocker

---
+++ diagram_updates_blocker.cpp
@@ -0,0 +1,44 @@
+/*****************************************************************************
+ * Copyright 2024 Serge Poltavski. All rights reserved.
+ *
+ * This file may be distributed under the terms of GNU Public License version
+ * 3 (GPL v3) as defined by the Free Software Foundation (FSF). A copy of the
+ * license should have been included with this file, or the project in which
+ * this file belongs to. You may also find the details of GPL v3 at:
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * If you have any questions regarding the use of this file, feel free to
+ * contact the author of this file, or the owner of the project in which
+ * this file belongs to.
+ *****************************************************************************/
+#include "diagram_updates_blocker.h"
+
+#include "diagram.h"
+
+namespace ceam {
+
+DiagramUpdatesBlocker::DiagramUpdatesBlocker(Diagram* diagram)
+    : diagram_(diagram)
+{
+    if (!diagram_)
+        return;
+
+    diagram_->blockSignals(true);
+    diagram_->setUpdatesEnabled(false);
+    view_mode_ = diagram_->viewportUpdateMode();
+    diagram_->setViewportUpdateMode(QGraphicsView::NoViewportUpdate);
+    diagram_->scene()->blockSignals(true);
+}
+
+DiagramUpdatesBlocker::~DiagramUpdatesBlocker()
+{
+    if (!diagram_)
+        return;
+
+    diagram_->scene()->blockSignals(false);
+    diagram_->setViewportUpdateMode(static_cast<QGraphicsView::ViewportUpdateMode>(view_mode_));
+    diagram_->setUpdatesEnabled(true);
+    diagram_->blockSignals(false);
+}
+
+} // namespace ceam
---
+++ diagram_updates_blocker.h
@@ -0,0 +1,32 @@
+/*****************************************************************************
+ * Copyright 2024 Serge Poltavski. All rights reserved.
+ *
+ * This file may be distributed under the terms of GNU Public License version
+ * 3 (GPL v3) as defined by the Free Software Foundation (FSF). A copy of the
+ * license should have been included with this file, or the project in which
+ * this file belongs to. You may also find the details of GPL v3 at:
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * If you have any questions regarding the use of this file, feel free to
+ * contact the author of this file, or the owner of the project in which
+ * this file belongs to.
+ *****************************************************************************/
+#ifndef DIAGRAM_UPDATES_BLOCKER_H
+#define DIAGRAM_UPDATES_BLOCKER_H
+
+namespace ceam {
+
+class Diagram;
+
+class DiagramUpdatesBlocker {
+    Diagram* diagram_ { nullptr };
+    int view_mode_ { 0 };
+
+public:
+    DiagramUpdatesBlocker(Diagram* diagram);
+    ~DiagramUpdatesBlocker();
+};
+
+} // namespace ceam
+
+#endif // DIAGRAM_UPDATES_BLOCKER_H
