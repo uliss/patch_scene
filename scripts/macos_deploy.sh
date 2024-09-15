8f9d446a6e279bb686877766beb3c722ccdbb9b9
Serge Poltavski <serge.poltavski@gmail.com>
2024-09-15 13:57:28 +0300

    Update macos_deploy.sh

--- scripts/macos_deploy.sh
+++ scripts/macos_deploy.sh
@@ -4,3 +4,4 @@ cmake --build "@CMAKE_BINARY_DIR@" --target git_version
 cmake --build "@CMAKE_BINARY_DIR@" --target all
 cmake --build "@CMAKE_BINARY_DIR@" --target dmg
 cmake --build "@CMAKE_BINARY_DIR@" --target app-zip
+cmake --build "@CMAKE_BINARY_DIR@" --target dev_upload
