51140a84efa13f42a2bfd7150fcf9788d57b5e2f
Serge Poltavski <serge.poltavski@gmail.com>
2024-10-28 12:18:46 +0300

    logging

--- undo_commands.cpp
+++ undo_commands.cpp
@@ -14,6 +14,7 @@
 #include "undo_commands.h"
 #include "diagram.h"
 #include "diagram_updates_blocker.h"
+#include "logging.hpp"
 
 namespace {
 constexpr int MoveDeviceId = 1000;
@@ -100,12 +101,16 @@ void DisconnectXlet::undo()
 {
     if (doc_ && id_.isValid())
         doc_->connectDevices(id_, view_data_);
+    else
+        WARN() << "can't connect:" << id_;
 }
 
 void DisconnectXlet::redo()
 {
     if (doc_ && id_.isValid())
         doc_->disconnectDevices(id_);
+    else
+        WARN() << "can't disconnect:" << id_;
 }
 
 RemoveDevice::RemoveDevice(Diagram* doc, const SharedDeviceData& data)
