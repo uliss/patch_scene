e2c1aa84321aac38b98e718ad8209a3d51dc79eb
Serge Poltavski <serge.poltavski@gmail.com>
2025-11-20 19:10:36 +0300

    comment class added

---
+++ comment.h
@@ -0,0 +1,32 @@
+/*****************************************************************************
+ * Copyright 2025 Serge Poltavski. All rights reserved.
+ *
+ * This file may be distributed under the terms of GNU Public License version
+ * 3 (GPL v3) as defined by the Free Software Foundation (FSF). A copy of the
+ * license should have been included with this file, or the project in which
+ * this file belongs to. You may also find the details of GPL v3 at:
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * If you have any questions regarding the use of this file, feel free to
+ * contact the author of this file, or the owner of the project in which
+ * this file belongs to.
+ *****************************************************************************/
+#ifndef COMMENT_H
+#define COMMENT_H
+
+#include "device.h"
+
+namespace ceam {
+
+class Comment : public Device {
+    Q_OBJECT
+public:
+    Comment();
+
+public:
+    void createContextMenu(QMenu& menu);
+};
+
+} // namespace ceam
+
+#endif // COMMENT_H
